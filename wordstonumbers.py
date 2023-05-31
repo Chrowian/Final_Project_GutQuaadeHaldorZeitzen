a2c903bed66b65c58417cd753ce28b964ccfb5c4
Chrowian <marcusgut@gmail.com>
2023-05-31 09:15:01 +0200

    idk words man

--- wordstonumbers.py
+++ wordstonumbers.py
@@ -1,17 +1,25 @@
 from itertools import chain
 from collections import Counter
 import numpy as np
+from torch import nn
+import torch
+device = "cuda" if torch.cuda.is_available() else "cpu"
 
 
-# 1. Get the vocabulary from the sentences
-x = [' I like apples ' ' I love bananas ' ' I like bananas ']
-words = 15
-
-def get_vocab(sentences):
-    all_words = list(chain(*[x.lower().split() for x in sentences]))
+def get_vocab(X):
+    senlist = X.values.tolist()[0:3000]
+    all_words = list(chain(*[i.lower().split() for i in senlist]))
+    n_words = 75000
     words, count = np.unique(all_words, return_counts=True)
-    idxs = np.argsort(count)[-words:]
+    idxs = np.argsort(count)[-n_words:]
     vocab = ['<UNK>'] + list(words[idxs][::-1])
-    print(vocab[:5], '...', vocab[-5:])
+    vocab_d = {vocab[i]: i for i in range(len(vocab))}
+    return vocab_d
 
-get_vocab(x)
+def sentence_to_integer_sequence(s, vocab):
+    return torch.tensor([vocab[x] if x in vocab else 0 for x in s.split()], dtype=torch.long)
+
+def sentence_to_tensor(s, vocab):
+    embedding = nn.Embedding(len(vocab), 50)
+    embedding.weight.data[0, :] = 0
+    return embedding(sentence_to_integer_sequence(s, vocab))

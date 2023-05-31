ea79ed4086337c33bc0f55377d39aae8b79b1d91
Chrowian <marcusgut@gmail.com>
2023-05-31 14:34:25 +0200

    made disruptions

--- wordstonumbers.py
+++ wordstonumbers.py
@@ -7,6 +7,9 @@ device = "cuda" if torch.cuda.is_available() else "cpu"
 
 
 def get_vocab(X):
+    """
+    :return: vocabulary dictionary of words and their corresponding indices
+    """
     senlist = X.values.tolist()[0:3000]
     all_words = list(chain(*[i.lower().split() for i in senlist]))
     n_words = 75000
@@ -17,9 +20,15 @@ def get_vocab(X):
     return vocab_d
 
 def sentence_to_integer_sequence(s, vocab):
+    """
+    :return: a sequence of integers corresponding to words in the sentence
+    """
     return torch.tensor([vocab[x] if x in vocab else 0 for x in s.split()], dtype=torch.long)
 
 def sentence_to_tensor(s, vocab):
+    """
+    :return: a tensor of word embeddings corresponding to words in the sentence
+    """
     embedding = nn.Embedding(len(vocab), 50)
     embedding.weight.data[0, :] = 0
     return embedding(sentence_to_integer_sequence(s, vocab))

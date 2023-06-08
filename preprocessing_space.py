ec0cbaca92ec1e7f166e620c052e3f5eb64e5135
RuneZeitzen <108991943+RuneZeitzen@users.noreply.github.com>
2023-06-08 11:18:24 +0200

    lightGBM finally works oh my lord

--- preprocessing_space.py
+++ preprocessing_space.py
@@ -2,6 +2,7 @@ import pandas as pd
 import nltk
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
+from scipy.sparse import csr_matrix, hstack
 
 nltk.download('stopwords')
 nltk.data.path.append("<path_to_nltk_data>")
@@ -33,3 +34,91 @@ def remove_stopwords(dataframe):
     nltk.data.path.append("<path_to_nltk_data>")
     df = dataframe.apply(remove_stop_words)
     return df
+
+
+
+from sklearn.feature_extraction.text import TfidfVectorizer
+from nltk.stem import PorterStemmer
+
+# for tree based classifiers
+
+def preprocess_text(df, text_columns, non_text_columns = None, max_features=5000):
+    """
+    Function to preprocess text columns and combine with other features in a DataFrame.
+    Args:
+        df (pandas.DataFrame): the input DataFrame.
+        text_columns (list of str): the names of the text columns to vectorize. ['title', 'text']
+        non_text_columns (list of str): the names of the non-text columns to include in the feature matrix.
+        max_features (int, optional): the maximum number of features for the TfidfVectorizer. Defaults to 5000.
+    Returns:
+        X (scipy.sparse.csr.csr_matrix): the combined feature matrix.
+        vectorizers (dict): dictionary mapping column names to trained TfidfVectorizers.
+    """
+
+    vectorizers = {}
+    feature_matrices = []
+
+    for col in text_columns:
+        vectorizer = TfidfVectorizer(max_features=max_features, stop_words='english')
+        X_col = vectorizer.fit_transform(df[col])
+        vectorizers[col] = vectorizer
+        feature_matrices.append(X_col)
+
+    if non_text_columns is not None:
+        for col in non_text_columns:
+            X_col = csr_matrix(df[col].values).T  # Transpose to get a column vector
+            feature_matrices.append(X_col)
+
+    X = hstack(feature_matrices)
+
+    return X, vectorizers
+
+
+# def preprocess_text_old(df, columns, use_stemming=True, max_features=5000):
+#     """
+#     Preprocesses the text data for the given dataframe and columns using optional stemming and TF-IDF vectorization.
+    
+#     Parameters
+#     ----------
+#     df : pandas.DataFrame
+#         The dataframe containing the text data.
+#     columns : list of str
+#         The columns in the dataframe to preprocess.
+#     use_stemming : bool, default=True
+#         If True, use stemming during the preprocessing.
+#     max_features : int, default=5000
+#         Maximum number of features for the TF-IDF vectorizer (i.e., maximum vocabulary size).
+
+#     Returns
+#     -------
+#     df_transformed : pandas.DataFrame
+#         The dataframe with the original non-text columns and the transformed text columns.
+#     vectorizers : dict
+#         A dictionary mapping column names to their corresponding TF-IDF vectorizers.
+#     """
+#     # Initialize stemmer
+#     stemmer = PorterStemmer()
+
+#     # Define the stemming function
+#     def stem_text(text):
+#         words = word_tokenize(text)
+#         words = [stemmer.stem(word) for word in words]
+#         return ' '.join(words)
+
+#     df_transformed = df.copy()
+#     vectorizers = {}
+
+#     for col in columns:
+#         if use_stemming:
+#             df_transformed[col] = df_transformed[col].apply(stem_text)
+
+#         # Initialize the vectorizer
+#         vectorizer = TfidfVectorizer(max_features=max_features)
+        
+#         # Fit and transform the vectorizer on our text
+#         df_transformed[col] = list(vectorizer.fit_transform(df_transformed[col]).toarray())
+        
+#         # Store the vectorizer
+#         vectorizers[col] = vectorizer
+
+#     return df_transformed, vectorizers

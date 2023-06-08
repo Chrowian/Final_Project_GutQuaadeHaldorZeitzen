2afc0662b2d1d8741df054d4be655427b03e74ee
RuneZeitzen <108991943+RuneZeitzen@users.noreply.github.com>
2023-06-08 19:08:22 +0200

    work

--- classifier_funcs.py
+++ classifier_funcs.py
@@ -22,6 +22,8 @@ import re
 import time
 import pickle
 import matplotlib.pyplot as plt
+import seaborn as sns
+
 
 ############################################# Helper Functions #############################################
 
@@ -171,7 +173,10 @@ from lightgbm import LGBMClassifier
 from lightgbm import early_stopping, log_evaluation
 from sklearn.metrics import accuracy_score, log_loss, confusion_matrix, roc_auc_score
 
-def LightGBM(X_train, y_train, X_test, y_test, early_stop = True):
+import matplotlib.pyplot as plt
+
+
+def LightGBM(X_train, y_train, X_test, y_test):
     """
     Trains a LightGBM binary classifier on the provided training data, 
     then makes predictions on the test data and evaluates the model's performance.
@@ -186,6 +191,8 @@ def LightGBM(X_train, y_train, X_test, y_test, early_stop = True):
         The test input samples.
     y_test : array-like of shape (n_samples,)
         The true values (class labels) for the test input samples.
+    plot : boolean, optional (default=False)
+        If True, plot a subplot with a confusion matrix and a ROC curve.
 
     Returns
     -------
@@ -231,4 +238,109 @@ def LightGBM(X_train, y_train, X_test, y_test, early_stop = True):
     roc_auc = roc_auc_score(y_test, y_pred_prob[:,1])
     print(f"\nROC AUC Score: {roc_auc}")
 
-    return accuracy, bce_loss, conf_matrix, roc_auc
+    return accuracy, bce_loss, conf_matrix, roc_auc, y_pred, y_pred_prob[:,1]
+
+
+####################
+
+def plot_params(ax, xlim = None, ylim = None, minor_grid = True, labsize = 12, xlocator = None, ylocator = None, log = (False,False)):
+
+    ax.minorticks_on()
+    ax.tick_params(axis='both', length = 10, which='major', labelsize=labsize)
+    ax.tick_params(axis='both', length = 5, which='minor', labelsize=labsize)
+    ax.grid(which = 'major',linestyle = ':',color = '0.25')
+    
+    if minor_grid:
+        ax.grid(which = 'minor',linestyle = ':',color = '0.75')
+
+    if xlim is not None:
+        ax.set_xlim(xlim)
+
+    if ylim is not None:
+        ax.set_ylim(ylim)
+
+    if xlocator is not None:
+        from matplotlib.ticker import AutoMinorLocator
+        minor_locator = AutoMinorLocator(xlocator)
+        ax.xaxis.set_minor_locator(minor_locator)
+    if ylocator is not None:
+        from matplotlib.ticker import AutoMinorLocator
+        minor_locator = AutoMinorLocator(ylocator)
+        ax.yaxis.set_minor_locator(minor_locator)
+
+
+    if log[0]:
+        ax.set_xscale('log')
+    if log[1]:
+        ax.set_yscale('log')
+
+####################
+
+def legend(ax, loc = 'lower left', size = 10, cols = None, fancy = False, shad = True, title = None, titsize = 12, bbox_to_anchor = None):
+    if bbox_to_anchor is None:
+        if title is None:
+            if cols is None:
+                ax.legend(loc=loc,prop={'size': size},framealpha = 1, facecolor = '1',edgecolor = 'black', shadow = shad, fancybox = fancy)
+            if cols is not None:
+                ax.legend(loc=loc,ncol = cols,prop={'size': size},framealpha = 1, facecolor = '1',edgecolor = 'black', shadow = shad, fancybox = fancy)
+    if bbox_to_anchor is not None:
+        if title is not None:
+            if cols is None:
+                ax.legend(loc=loc,prop={'size': size},framealpha = 1, facecolor = '1',edgecolor = 'black', shadow = shad, fancybox = fancy, title = title, title_fontsize = titsize, bbox_to_anchor = bbox_to_anchor)
+            if cols is not None:
+                ax.legend(loc=loc,ncol = cols,prop={'size': size},framealpha = 1, facecolor = '1',edgecolor = 'black', shadow = shad, fancybox = fancy,title = title, title_fontsize = titsize, bbox_to_anchor = bbox_to_anchor)
+
+####################
+
+def plot_text(ax, x, y, string, size = 12, box = True, color = 'k', font_style = 'normal'):
+    if box:
+        return ax.text(x, y, string,c = color,size = size,bbox=dict(boxstyle='Square', facecolor='white', alpha=1), family='monospace', fontstyle = font_style)
+    else:
+        return ax.text(x, y, string,c = color,size = size, family='monospace', fontstyle = font_style)
+
+# Plot the confusion matrix and ROC curve
+def plot_confusion_matrix_and_roc(y_test, y_pred, y_pred_probs, name, vobab, type = 'LightGBM', data = None):
+
+    font = {'family': 'monospace'}
+    plt.rc('font', **font)
+
+    # Compute the confusion matrix
+    conf_matrix = confusion_matrix(y_test, y_pred)
+
+    # Compute ROC curve and ROC area
+    fpr, tpr, _ = roc_curve(y_test, y_pred_probs)
+    roc_auc = auc(fpr, tpr)
+
+    # Create a figure
+    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 5))
+
+    fig.suptitle(f'Confusion Matrix and ROC Curve\nBest {type} Model', fontsize=20, y = 1.05)
+
+    # Plot confusion matrix
+    sns.heatmap(conf_matrix, cmap='Blues', annot=True, fmt="d", ax=ax1, vmin = 0)
+    #ax1.set_title('Confusion Matrix')
+    ax1.set_xlabel('Predicted Label')
+    ax1.set_ylabel('True Label')
+
+    
+
+    # Plot ROC curve
+    ax2.plot(fpr, tpr, color='b', label='ROC curve (area = %0.2f)' % roc_auc)
+    ax2.plot([0, 1], [0, 1], color='k', linestyle='--')
+    ax2.set_xlim([-0.01, 1.01])
+    ax2.set_ylim([-0.01, 1.01])
+    ax2.set_xlabel('False Positive Rate')
+    ax2.set_ylabel('True Positive Rate')
+    #ax2.set_title('Receiver Operating Characteristic')
+    if data is None:
+        legend(ax2,loc='lower right')
+    plot_params(ax2)
+
+    if data is not None:
+        string = f'Accuracy: {data[0]:.4f}\nBCE (LogLoss): {data[1]:.4f}\nROC AUC: {data[2]:.3f}\n\nVobaulary Size: {data[3]}'
+        plot_text(ax2, 0.5, 0.1, string, size = 12, box = True, color = 'k', font_style = 'italic')
+
+    # Save the figure
+    plt.savefig(f'Result_Models/evaluation_{name}_{vobab}.png', dpi=300, bbox_inches='tight')
+
+    plt.show()
